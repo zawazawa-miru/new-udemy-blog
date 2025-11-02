97ff82ee266e243befe01dae282bd1b04ce1cc59
zawazawa-miru <riyusa2020@gmail.com>
2025-11-02 05:46:21 +0000

    67完了

---
+++ src/auth.ts
@@ -0,0 +1,32 @@
+import NextAuth from 'next-auth';
+import { authConfig } from './auth.config';
+import Credentials from 'next-auth/providers/credentials';
+import { z } from 'zod';
+import { prisma } from '@/lib/prisma'; 
+import bcryptjs from 'bcryptjs';
+ 
+async function getUser(email: string) { // ユーザー取得関数 
+  return await prisma.user.findUnique({ 
+    where: { email: email }})}
+
+export const { auth, signIn, signOut, handlers } = NextAuth({
+  ...authConfig,
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(8) })
+          .safeParse(credentials);
+
+          if (parsedCredentials.success) { 
+          const { email, password } = parsedCredentials.data; 
+          const user = await getUser(email); // ユーザー取得 
+          if (!user) return null; 
+          const passwordsMatch = await bcryptjs.compare(password, user.password); // パスワード比較 
+          if (passwordsMatch) return user; 
+        } 
+        return null;
+      },
+    }),
+  ],
+});
