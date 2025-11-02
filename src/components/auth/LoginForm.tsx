ad4cd5c311555738b7ada124d272c5c168868852
zawazawa-miru <riyusa2020@gmail.com>
2025-11-02 09:45:15 +0000

    71完了

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -17,5 +17,33 @@ export default function LoginForm() {
     authenticate,
     undefined,
   )
-  
+  return(
+    <Card>
+      <CardHeader>
+        <CardTitle>ログイン</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <form action={formAction} className="space-y-4">
+          <div className="space-y-4">
+            <Label htmlFor="email">メールアドレス</Label>
+            <Input id="email" type="email" name="email" required />
+          </div>
+          <div className="space-y-4">
+            <Label htmlFor="password">パスワード</Label>
+            <Input id="password" type="password" name="password" required />
+          </div>
+          <Button type="submit" className="w-full">ログイン</Button>
+          <div
+            className="flex h-8 items-end space-x-1"
+          >
+            {errorMessage && (
+              <div className="text-red-500">
+                <p className="text-sm text-red-500">{errorMessage}</p>
+              </div>
+            )}
+          </div>
+        </form>
+      </CardContent>
+    </Card>
+  )
 }
